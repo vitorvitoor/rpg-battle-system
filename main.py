01f4a3e39e0d565143fdef8fbf4cdf980424d15b
Vitor Almeida Machado <almeida.vitor443@gmail.com>
2026-01-26 22:30:52 -0300

    Criacao de classe e objeto soldados e seus respectivos valores

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#Configurando workspace e ajustando ambiente para inicio de desenvolvimento.
+class Soldado:
+    def __init__(self, nome_inicial, municao_inicial):
+        self.nome = nome_inicial      
+        self.municao = municao_inicial
+
+soldado_americano = Soldado("Chris Evans", ".38 SPL")
+
+soldado_frances = Soldado("Jerry Cabannes", ".45 ACP")
+
+print(vars(soldado_americano))
+print(vars(soldado_frances))
