d5044c293e34a8825d0576dfc1584c544bfcae6f
Vitor Almeida Machado <almeida.vitor443@gmail.com>
2026-02-15 16:10:40 -0300

    Criação de lista com os recrutas e seus atributos

--- training_camp.py
+++ training_camp.py
@@ -1,22 +1,19 @@
 from Models.training_camp_models import Recruta
 
-dave = Recruta("David Johnson", 10)
-bryan = Recruta("Bryan Smith", 10)
-liam = Recruta("Liam Scott", 10)
+squad = [Recruta("David", 10),
+        Recruta("Bryan", 10),
+        Recruta("Liam", 10),
+        Recruta("Ben", 10),
+        Recruta("Victor", 10)]
 
-print("--- INÍCIO DO TREINAMENTO ---")
-print(dave) 
-print(bryan)
-print(liam)
+print("--- INÍCIO DO TREINAMENTO - 10KM ---")
+
+for recruta in squad:
+    recruta.run(10)
 print("-" * 30) #Linha divisória visual
 
-dave.run(2)
-bryan.run(6)
-liam.run(8)
-print("-" * 30)
-
 print("--- RELATÓRIO FINAL ---")
-print(dave)
-print(bryan)
-print(liam)
-print(Recruta.contagem_tropas())
+print(Recruta.contagem_tropas(), "")
+
+
+
