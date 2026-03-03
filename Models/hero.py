80bf3a0176f4eb08032c3faca4c5646b6715f599
Vitor Almeida Machado <almeida.vitor443@gmail.com>
2026-03-03 11:17:37 -0300

    feat: initial game structure and hero class fixed with corrections

--- Models/hero.py
+++ Models/hero.py
@@ -10,28 +10,7 @@ class Hero:
         self.attack = attack
 
         Hero.total_heroes += 1
-
-    @classmethod #affect all the heroes because is acting directly on the class
-    def troops_count(cls): 
-        return f"We have {cls.total_heroes} heroes on the field!"
-    
-    def __str__(self):
-        return f"[{self.combat_class}] {self.name} | Energy: {self.hp}/{self.max_hp}"
-
-    def run(self, total_distance):
-        if total_distance > self.hp:
-            print(f"{self.name} tryed to run {total_distance}km but passed away of exausthion...")
-            self.hp = 0
-        else:
-            self.hp -= total_distance
-            print(f"{self.name} runned! current hp: {self.hp}")
-
-    def rest(self):
-        self.hp += 5
-        if self.hp > self.max_hp:
-            self.hp = self.max_hp
-            print(f"{self.name} have rested! Actual stamina: {self.hp}")
-
+  
     def attack_enemy(self):
         return f"The hero {self.name} attacked causing X damage."
   
--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 from models.hero import Hero
 
-int(input("What's the first player's name? "))
+players_name = input("What's the player's name? ")
 
-player = Hero("Koukusen", 100, "Warrior", 8)
+print("What's the player's class:\n[1] Warrior\n[2] Wizard\n[3] Archer\n[4] Dark Elf\n[5] High Elf")
+player_class_choice = input("Type your choice here: ----> ")
 
-contador = 0 
+if player_class_choice == "1":
+    chosen_class = "Warrior"
+    base_hp = 100
+    base_attack = 7
+
+elif player_class_choice == "2":
+    chosen_class = "Wizard"
+    base_hp = 70  
+    base_attack = 10
+
+elif player_class_choice == "3":
+    chosen_class = "Archer"
+    base_hp = 65
+    base_attack = 12
+
+elif player_class_choice == "4":
+    chosen_class = "Dark Elf"
+    base_hp = 85  
+    base_attack = 9
+
+elif player_class_choice == "5":
+    chosen_class = "High Elf"
+    base_hp = 80
+    base_attack = 10
+
+else:
+    print("Invalid choice! You are now a Peasant.")
+    chosen_class = "Peasant"
+    base_hp = 30
+    base_attack = 2
+
+player = Hero(player_class_choice, base_hp, chosen_class, base_attack)
 
 while True:
      
-    print("--- !!! WELCOME TO RPG BATTLE SYSTEM !!! ---")
+    print("\n--- !!! WELCOME TO RPG BATTLE SYSTEM !!! ---")
     print("'1' Attack" '\n' "'2' Quit")
 
     player_choice = input("Type you choice here: ----> ")
@@ -16,14 +48,10 @@ while True:
     if player_choice == "1":
         player.attack_enemy()
 
-    elif player_choice == "2" or player_choice == quit:
-        print("Saindo do jogo...")
+    elif player_choice == "2" or player_choice == "quit":
+        print("Shutting down...")
+        break
 
     else:
-        print("Comando inválido. Tente novamente!")
+        print("Invalid command. Try again!")
         print("-" * 20)
-
-
-
-
-
